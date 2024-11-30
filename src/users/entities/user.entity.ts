4ef60ff4bb4186841f259461da3d76a75a758511
FOREST Benjamin <benjamin.frt28@gmail.com>
2024-11-30 15:47:38 +0100

    add seeding users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('user_')
-export class User{
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 30, nullable: false })
+  @Column({ type: 'varchar', length: 30, nullable: false, default: 'adrien' })
   pseudo: string;
 
-  @Column({ type: 'varchar', length: 43, nullable: false })
+  @Column({ type: 'varchar', length: 43, nullable: false, default: 'password' })
   password: string;
 }
